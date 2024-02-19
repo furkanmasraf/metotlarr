aaccc47e140093c81a616db9d4f7495e7993b5fa
furkanmasraf <furkanmasraf@FURKANMASRA088F>
2024-02-19 16:50:53 +0300

    Add project files.

---
+++ metotlarr/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace metotlarr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getir()
+        {
+            textBox1.Text = "Furkan";
+            textBox2.Text = "Masraf";
+            textBox3.Text = "Bilgisayar Muhendisi";
+            textBox4.Text = "Uskudar Universitesi";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getir();
+        }
+    }
+}
